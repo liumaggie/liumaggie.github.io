6293c3053d0af40d83d7cf52bd80ab7fa98fab1b
Maggie Liu <mliu0805@gmail.com>
2021-09-28 18:52:09 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-99764363-1",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
